8261410275c2ba98f603fa2645ed93cbd0f6de72
matteda ramesh <mattedaramesh27@gmail.com>
2023-06-16 19:02:51 +0530

    adding login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './App.css';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et molestias reiciendis quos similique vero natus blanditiis necessitatibus quis voluptatum dolorem fugit accusamus omnis pariatur, ratione minima explicabo voluptatibus neque iure.</p>
+      <br />
+      <Login />
     </div>
   );
 }
